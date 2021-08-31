01453ccd7eae87544fb975e7255baa9c01b7935d
David Salazar <davidjosesalazarmora@gmail.com>
2021-08-31 10:42:13 -0500

    back

--- app/urls.py
+++ app/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from repartidores_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('repartidores/', views.Repartidores.as_view()),
+    path('asignacion/', views.AsignarRepartidor.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- repartidores_app/views.py
+++ repartidores_app/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from .models import RepartidoresModel
+from .serializers import RepartidoresSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.db import connection
+from django.shortcuts import redirect
 
-# Create your views here.
+
+
+
+class Repartidores(APIView):
+    def get(self, request, format=None):
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT COUNT(*) FROM repartidores_app_repartidoresmodel 
+                WHERE activo = TRUE;""")
+            row = cursor.fetchone()
+            data = {'activos': row[0]}
+        return Response(data)
+
+def request_value(request):
+    if request.data['data']:
+        value = 1
+        return value
+    else:
+        value = 2
+        return value
+
+class AsignarRepartidor(APIView):
+    def post(self, request, format=None):
+        with connection.cursor() as cursor:
+            value = request_value(request)
+            cursor.execute("""UPDATE repartidores_app_repartidoresmodel 
+                SET activo= NOT activo WHERE iD=(case {value} 
+                    WHEN 1 THEN (SELECT MAX(iD) FROM repartidores_app_repartidoresmodel 
+                        WHERE activo = TRUE) 
+                    WHEN 2 THEN (SELECT MIN(iD) FROM repartidores_app_repartidoresmodel  
+                        WHERE activo = FALSE) END)""".format(value = value))
+        return redirect('/repartidores/')
+
+    
